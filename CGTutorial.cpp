9e5c388a10881a82314eec2fd9e3195214f56240
NanashiGIT <s0545045@htw-berlin.de>
2015-06-21 18:00:36 +0200

    Implemented Controls

--- CGTutorial.cpp
+++ CGTutorial.cpp
@@ -12,6 +12,8 @@
 
 // Include GLFW
 #include <GLFW/glfw3.h>
+GLFWwindow* window;
+#include "controls.hpp"
 
 // Include GLM
 #include <glm/glm.hpp>
@@ -29,95 +31,6 @@ using namespace glm;
 #include "objloader.hpp"
 
 #include "texture.hpp"
-
-
-
-// Erklären ObenGL-Statemachine, lowlevel
-// Version 1: seit 1992, elegantes API für die Plattformunabhägige 3D-Programmierung 
-// Version 2: seit 2002, Ergänzung durch Shader-Programme, die auf Grafikkarte laufen
-// Version 3: seit 2008, Fixedfunction-Pipeline nur noch als Compability-Extension
-// Version 4: seit 2010 noch kaum unterstützt
-// Wir setzen nun auf Version 3, schwieriger zu erlernen als Version 1
-// glm-Bibliothek für Matrix-Operationen (fehlen in Version3), glew-Bibliothek, um API-Funktionen > 1.1 nutzen zu können
-// glfw-Bibliothek, um OpenGL plattformunabhängig nutzen zu können (z. B. Fenster öffnen)
-#define UEBUNG1 /* feste Rotation */
-// Pflichtteil: Ergebnis zeigen ...
-// Kode zeilenweise erklären. (Orientiert sich an http://www.opengl-tutorial.org/, Evtl. diese Tutorials dort ausführen, um zu vertiefen.)
-// Dort wird glfw in der Version 2 genutzt, wir verwenden die aktuelle Version 3 http://www.glfw.org/
-// Schwarze Linien wg. Shader. Kurz erklaeren, was die Shader machen...
-// Vorgehensweise erklaeren, Jeweils alte cpp-Datei nach CGTutorial.cpp.n kopieren, damit jede Aenderung nachvollziehbar bleibt.
-// (Voraussetzung fuer Support)
-// Aufgabe Rotation: glm::rotate    (http://glm.g-truc.net/glm.pdf)
-#define UEBUNG2 /* variable Rotation ueber R-Taste */
-// Eventloop, kann man muss man aber nicht in glfw (Alternativ Glut, dort muss man)
-// Aufgabe: Hinweis globale Variable, Taste...
-#define UEBUNG3 /* Kamerasteuerung */
-// Aufgabe drei Unabhängige Rotationen, wird zu erstem Teil des Pflichtteils
-// Hinweis auf andere Kaperasteuerungen ->  http://www.opengl-tutorial.org
-#define UEBUNG4 /* Ausgemaltes Objekt und Z-Buffer */
-// OpenGL-Befehle: http://wiki.delphigl.com/index.php/Hauptseite auf Deutsch!
-// GLEW http://glew.sourceforge.net/
-// Wireframe vs. Solid thematisieren, Z-Buffer wird noetig, um Verdeckungsproblem zu lösen
-// http://www.opengl-tutorial.org/beginners-tutorials/tutorial-4-a-colored-cube/
-///////////////////////////////////////////////////////
-#define UEBUNG5 /* Einlesen eines Objekts */
-// http://www.opengl-tutorial.org/beginners-tutorials/tutorial-7-model-loading/
-// Bessere OBJ-Loader vorhanden, basieren aber auf OpenGL1.1
-// Andere Formate möglich, z. B. 3ds, s. a. Link am Ende der Seite
-// Wir nehmen Teapot, Anekdote zu Teapot:  von Newell 1975 (hat berühmtes Lehrbuch geschrieben) glut, 3dsmax, Toystory, Monster AG, ...
-//
-// Vertex Buffer Objects (VBO) (ab 1.5) enthalten alle Daten zu Oberflaechen eines Objekts (Eckpunktkoordinaten, Normalen, Texturkoordinaten -> Uebung 6)
-// koennen in separatem Speicher einer Grafikkarte gehalten werden (haengt vom Modell ab)
-// koennen ueber API-Aufrufe veraendert werden --> glBufferData, glMapBuffer (GL_STATIC_DRAW, um mitzuteilen, dass sich nichts aendern wird)
-// Vertex Array Objects (VAO) (ab 3) kapseln mehrere VBOs eines Objects zwecks Optimierung und einfachrer Benutzung:
-// Beim Zeichnen nur 2 Aufrufe: glBindVertexArray und glDrawArrays
-//#define UEBUNG5TEDDY  /* Nach der Teddy-Übung fuer Uebung6 wieder ausschalten */
-// Alternativ: Teddy wenn die Zeit reicht: google nach teddy chameleon, teapot.obj mit xxx.obj austauschen....
-// Modellieren kann also auch einfach sein, Freies Tool Blender (open Source), Professionelle Werkzeuge Maya, 3dsmax, etc. (nicht gegenstand der LV)
-#define UEBUNG6 /* Beleuchten, neuer Shader */
-// Teddy-Modell hat keine Normalen, passt nicht zu Shadern, wieder zur Teekanne zurueck.
-// Shader anschauen, Alter Shader "ColourFragmentShader" setzt Farbe direkt, wird interpoliert ("Gouraud-Shading")
-// "TransformVertexShader" gibt Farben weiter, legt layout der Eingabe-Daten fest, verwendet MVP um Eckpunkte zu transvormieren ("Eine Matrix reicht")
-// Neue Shader koennen mehr (Immer noch nicht, was die Fixed-Function-Pipeline konnte) 
-// Normalen stehen senkrecht auf Oberflaechen, wichtig fuer die Beleuchtung
-// Normalen muessen anders behandelt werden als Eckpunkte, deshalb Trennung von MVP in Model View Projection
-// -> Shader verändern in Uebung 15
-#define UEBUNG7 /* Texturieren */
-// Farbtexturen sind digitale Rasterbilder (andere Texturen in Kombination moeglich "Multi-Texturing" nicht Gegenstand der Uebung -> VL Textur)
-// Imageloader fuer png und jpg nicht in den Bibliotheken enthalten -> SOIL
-// DDS-Format kompatibel zu Texturkompression der Hardware. Wir nehmen aber BMP !
-// s. a.:  http://www.opengl-tutorial.org/beginners-tutorials/tutorial-5-a-textured-cube/, Achtung glfwLoadTexture2D gibt' nicht mehr in glfw-version 3
-// Granularitaet: Textur pro Objekt. kann also gewechselt werden ! --> Uebung 8
-// Texturierte Teekanne ist 2. Teil des Pflichtteils 
-///////////////////////////////////////////////////////
-#define UEBUNG8 /* Mehrere Objekte */
-// Polygone bilden Objekt, Objekte haben eigene Transformationen ("Model"-Matrix) eigene Texturen, etc.
-// VAOs abwechselnd binden, MVP-Matrizen abwechselnd setzen, ggf Texturen abwechselnd binden, OpenGL verwendet jeweils positive Zahlen als
-// Namen, die man sich generieren lassen muss.
-// Model-Matrix fuer jedes Objekt anders, Szenen haben meist hierarchische Struktur, Multiplikationskette von der Wurzel zum Blatt
-// Roboter-Beispiel, um Ketten zu lernen
-#define UEBUNG9 /* Koordinatenachse */
-// Notwendigkeit Koordinatensysteme anzeigen zu lassen... -> drawCS-Funktion mit drei Balkenen
-// Wie erhaelt man langen duennen Balken mit bekannten Geometrien ?
-// Aufgabe: Balken (leider mit M, V, P, ... als Parameter) Hinweis ginge auch als Singleton...
-// Skalierungsparameter ruhig ausprobieren.
-// sieht man den Balken im Bauch der Teekanne ?
-#define UEBUNG10 /* Koordinatenkreuz */
-// Aufgabe: Drei Balken, spaeter entsteht die Notwendigkeit Matrizen zu sichern (evtl. Mechanismus von OpenGL1 erwaehnen)
-#define UEBUNG11 /* Ein Robotersegment */
-// Teekanne ausblenden, Kugel zeichnen, Transformationsreihenfolge nochmal thematisieren
-// Aufgabe: -Funktion mit Parameter!
-#define UEBUNG12 /* Drei Robotersegmente uebereinander */
-// Aufgabe: statischer Roboter, Unterschied lokales vs. globales Translate
-#define UEBUNG13 /* Rotationen in den Gelenken */
-// Aufgabe Roboter mit Tastensteuerung, Reihenfolge der Transformationen von oben nach unten und umgekehrt mit Stiften erläutern
-#define UEBUNG14 /* Lampe am Ende */
-// Uebung 15 im StandardShading pixelshader
-// Hier mal exemplarisch den Pixelshader aendern, um die Beleuchtung ansprechender zu machen
-// Erwaehnen, dass man Parameter wie "MVP" kontrollieren koennte.
-// Hier beginnt die Wellt der Shader-programmierung, die nicht Gegenstand der Uebung ist.
-// Lampe am Ende eines steuerbaren Roboters ist dritter Teil des Pflichtteils
-// (5 Punkte: 3 Rotationen, Teekanne, texturiert, Roboter, Licht am Ende) 
 
 #define groesse 1.0
 
@@ -237,7 +150,7 @@ static void drawLevel(){
 				Model = save;
 				
 			}
-			Model = glm::mat4(1.0f);
+			//Model = glm::mat4(1.0f);
 			cout << level[i][j];
 			
 		}
@@ -274,9 +187,9 @@ int main(void)
 
 	// Open a window and create its OpenGL context
 	// glfwWindowHint vorher aufrufen, um erforderliche Resourcen festzulegen
-	GLFWwindow* window = glfwCreateWindow(1024, // Breite
+	window = glfwCreateWindow(1024, // Breite
 		768,  // Hoehe
-		"CG - Tutorial", // Ueberschrift
+		"CG Beleg", // Ueberschrift
 		NULL,  // windowed mode
 		NULL); // shared window
 
@@ -301,9 +214,16 @@ int main(void)
 
 	// Auf Keyboard-Events reagieren
 	glfwSetKeyCallback(window, key_callback);
-
+	glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+	glfwSetCursorPos(window, 1024 / 2, 768 / 2);
+	// Enable depth test
+	glEnable(GL_DEPTH_TEST);
+	// Accept fragment if it closer to the camera than the former one
+	glDepthFunc(GL_LESS);
 	// Dark blue background
 	glClearColor(100.0f, 100.0f, 100.0f, 0.2f);
+	// Cull triangles which normal is not towards the camera
+	glEnable(GL_CULL_FACE);
 
 	// Create and compile our GLSL program from the shaders
 	programID = LoadShaders("StandardShading.vertexshader", "StandardShading.fragmentshader");
@@ -311,8 +231,7 @@ int main(void)
 
 	// Shader auch benutzen !
 	glUseProgram(programID);
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS);
+	
 
 
 	std::vector<glm::vec3> vertices;
@@ -368,18 +287,26 @@ int main(void)
 		glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);	
 		
 		// Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-		Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+		//Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+
+		// Compute the MVP matrix from keyboard and mouse input
+		computeMatricesFromInputs();
+		Projection = getProjectionMatrix();
+		View = getViewMatrix();
+		Model = glm::mat4(1.0);
+		glm::mat4 MVP = Projection * View * Model;
+
 
 		// Camera matrix
-		View = glm::lookAt(glm::vec3(10,20,5), // Camera is at (0,0,-5), in World Space
-			glm::vec3(5,5,5),  // and looks at the origin
-			glm::vec3(0,1,0)); // Head is up (set to 0,-1,0 to look upside-down)
+		//View = glm::lookAt(glm::vec3(10,20,5), // Camera is at (0,0,-5), in World Space
+		//	glm::vec3(5,5,5),  // and looks at the origin
+		//	glm::vec3(0,1,0)); // Head is up (set to 0,-1,0 to look upside-down)
 
 		/*glm::vec3 lightPos = glm::vec3(4,4,-4);
 		glUniform3f(glGetUniformLocation(programID, "LightPosition_worldspace"), lightPos.x, lightPos.y, lightPos.z);*/
 
 		// Model matrix : an identity matrix (model will be at the origin)
-		Model = glm::mat4(1.0f);
+		// = glm::mat4(1.0f);
 		//Objekt drehen
 		Model = glm::rotate(Model, x, glm::vec3(1,0,0));
 		Model = glm::rotate(Model, y, glm::vec3(0,1,0));
@@ -388,13 +315,11 @@ int main(void)
 		
 		glm::vec4 lightPos = Model * glm::vec4(10.0,10.0,10.0,1.0);
 		glUniform3f(glGetUniformLocation(programID, "LightPosition_worldspace"), lightPos.x, lightPos.y, lightPos.z);
-		//drawWireCube();	
-		//drawCube();		
-
 		
 		drawLevel();
 		// Bind our texture in Texture Unit 0
-		
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, Texture);
 
 		// Set our "myTextureSampler" sampler to user Texture Unit 0
 		glUniform1i(glGetUniformLocation(programID, "myTextureSampler"), 0);
